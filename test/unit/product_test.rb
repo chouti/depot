88bf5eb67aea9736f18d48e1dc7ecf1831d2ee57
chouti <chouti@gmail.com>
2011-04-01 07:41:57 +0800

    add unit test case when price was 1 should be valid price

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -24,5 +24,8 @@ class ProductTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal "must be greater than or equal to 0.01",
       product.errors[:price].join('; ')
+    
+    product.price = 1
+    assert product.valid?
   end
 end
