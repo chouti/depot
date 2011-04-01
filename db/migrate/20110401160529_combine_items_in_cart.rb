88cdfc3d1e789d2cc0a54d28dd304689559a26de
chouti <chouti@gmail.com>
2011-04-02 07:34:58 +0800

    fix typo in combine_items_in_cart migration

--- db/migrate/20110401160529_combine_items_in_cart.rb
+++ db/migrate/20110401160529_combine_items_in_cart.rb
@@ -5,12 +5,19 @@ class CombineItemsInCart < ActiveRecord::Migration
       sums.each do |product_id, quantity|
         if quantity > 1
           cart.line_items.where(:product_id => product_id).delete_all
-          cart.line_items.create(:product_id => product_id, :quantty => quantity)
+          cart.line_items.create(:product_id => product_id, :quantity => quantity)
         end
       end
     end
   end
 
   def self.down
+    LineItem.where("quantity>1").each do |line_item|
+      line_item.quantity.times do
+        LineItem.create :cart_id => line_item.cart_id,
+        :product_id => line_item.product_id, :quantity =>1
+      end
+      line_item.destroy
+    end
   end
 end
