f5edf7948aa679867c90797669f34c8afae84563
chouti <chouti@gmail.com>
2011-04-03 14:46:15 +0800

    add shipment mail text

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -17,9 +17,9 @@ class Notifier < ActionMailer::Base
   #
   #   en.notifier.order_shipped.subject
   #
-  def order_shipped
-    @greeting = "Hi"
+  def order_shipped(order)
+    @order = order
 
-    mail :to => "to@example.org"
+    mail :to => order.email, :subject => "Pragmatic Store Order Shipped"
   end
 end
